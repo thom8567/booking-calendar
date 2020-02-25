3574cd48c0984b4636f9ecfa9383a40270876fae
Tom Ford-Knowles <thomas.ford-knowles@hotmail.co.uk>
2020-02-25 13:57:55 +0000

    Refactor

---
+++ calendar.js
@@ -0,0 +1,201 @@
+(function() {
+  function getEvents(callback) {
+    $.get('/scraper.php', function(returnedData) {
+      callback(JSON.parse(returnedData));
+    }).fail(function() {
+      alert('Events could not be retrieved!')
+    });
+  }
+
+  function getColoursFromStatus(status) {
+    const MAP = {
+      'Cancelled': ['#cc0000', 'white'],
+      'Suspended': ['#ffa31a', 'black'],
+      'Closed for Bookings': ['#003e80', 'white'],
+      'Closed for Entries': ['#ff1a1a', 'black'],
+      'Accepting Bookings': ['#007bff', 'white'],
+      'Open for Entries': ['#009933', 'black'],
+      'Fully Closed': ['#801a00', 'white'],
+      'Planning': ['#d1e0e0', 'black'],
+      '_default': ['#007bff', 'white']
+    };
+
+    return MAP[status] || MAP._default;
+  }
+
+  function addEventsToCalendar(events) {
+    Object.values(events).forEach(function (item) {
+      //if there is no title or no date then do not attempt to render the event
+      if (!item['title'] || !item['date']) {
+        return;
+      }
+      const [
+        colour,
+        textColour
+      ] = getColoursFromStatus(item['status']);
+      calendar.addEvent({
+        title: item['title'],
+        start: item['date'],
+        dateOfEvent: convertToLongDate(item['date']),
+        category: item['category'] || false,
+        region: item['region'] || false,
+        status: item['status'] || false,
+        booking_deadline: item['booking_deadline'] || false,
+        planned_closing_date: item['planned_closing_date'] || false,
+        color: colour,
+        textColor: textColour,
+        editable: false,
+      });
+    });
+    showCalendar();
+  }
+
+  function showCalendar() {
+    $("#loaderContainer").hide();
+    $("html").removeClass("loading");
+  }
+
+  function getTodaysDate() {
+    let today = dayjs(new Date());
+
+    return today.format('DD/MM/YYYY');
+  }
+
+  function convertToLongDate(dateToFormat) {
+    let date = dayjs(new Date(dateToFormat));
+
+    return date.format('dddd, D MMMM YYYY');
+  }
+  var calendar;
+
+  $(function() {
+    //JQuery Selectors
+    var $modalEventTitle = $("#eventTitle");
+    var $modalEventDate = $("#eventDate");
+    var $modalEventRegion = $("#eventRegion");
+    var $modalEventCategory = $("#eventCategory");
+    var $modalEventStatus = $("#eventStatus");
+    var $modalEventDeadline = $("#eventDeadline");
+    var $modalEventClosingDate = $("#eventClosing");
+    var $eventModal = $("#eventModal");
+
+    var calendarEl = document.getElementById('calendar');
+
+    calendar = new FullCalendar.Calendar(calendarEl, {
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+      plugins: ['resourceDayGrid', 'interaction', 'bootstrap'],
+      themeSystem: 'bootstrap',
+      customButtons: {
+        nextYearButton: {
+          bootstrapFontAwesome: 'fa-angle-double-right',
+          click: function() {
+            calendar.nextYear();
+          }
+        },
+        prevYearButton: {
+          bootstrapFontAwesome: 'fa-angle-double-left',
+          click: function() {
+            calendar.prevYear();
+          }
+        },
+        resetButton: {
+          text: 'Reset',
+          click: function() {
+            let date = dayjs(calendar.getDate()).format('YYYY-MM');
+            $(`[data-date*="${date}"].availabilityMarker`).remove();
+          }
+        }
+      },
+      header: {
+        center: 'resetButton',
+        right: 'prevYearButton,prev,today,next,nextYearButton',
+      },
+      buttonText: {
+        today:  'Today: ' + getTodaysDate(),
+        month:  'Month',
+        week:   'Week',
+        day:    'Day',
+        list:   'List'
+      },
+      firstDay: 1,
+      eventOrder: function(a, b) {
+        let statusA = a['status'];
+        let statusB = b['status'];
+        let priorities = {
+          'Cancelled': 10,
+          'Suspended': 9,
+          'Fully Closed': 8,
+          'Closed for Entries': 7,
+          'Closed for Bookings': 6,
+          'Open for Entries': 5,
+          'Accepting Bookings': 4,
+          'Planning': 3
+        };
+        if (!(statusA in priorities) || !(statusB in priorities)){
+          return -1;
+        }
+        let aPriority = priorities[statusA];
+        let bPriority = priorities[statusB];
+        if (aPriority > bPriority) {
+          return -1;
+        }
+        if (aPriority < bPriority) {
+          return 1;
+        }
+      },
+      eventClick: function(info) {
+        let event = info['event'];
+        $modalEventTitle.html(event.title);
+
+        $modalEventStatus.html('Status: ' + event.extendedProps.status);
+        $modalEventStatus.toggle(!!event.extendedProps.status);
+
+        $modalEventDate.html('Date: ' + event.extendedProps.dateOfEvent);
+
+        $modalEventRegion.html('Region: ' + event.extendedProps.region);
+        $modalEventRegion.toggle(!!event.extendedProps.region);
+
+        $modalEventCategory.html('Category: ' + event.extendedProps.category);
+        $modalEventCategory.toggle(!!event.extendedProps.category);
+
+        $modalEventDeadline.html('Booking Deadline: ' + event.extendedProps.booking_deadline);
+        $modalEventDeadline.toggle(!!event.extendedProps.booking_deadline);
+
+        $modalEventClosingDate.html('Planned Closing Date: ' + event.extendedProps.planned_closing_date);
+        $modalEventClosingDate.toggle(!!event.extendedProps.planned_closing_date);
+
+        $eventModal.modal();
+      },
+      dateClick: function(info) {
+        let element = info['dayEl'];
+        let date = info['dateStr'];
+        let clickedDate = new Date(date);
+        let calendarDate = calendar.getDate();
+        let $crossSelector = $('[data-date="' + date + '"].fa-times');
+        let $tickSelector = $('[data-date="' + date + '"].fa-check');
+
+        if (clickedDate < calendarDate) {
+          return;
+        }
+        //On first click add a Tick
+        //On second click remove Tick and add a cross
+        //On third click remove the cross to leave the day blank
+        if ($tickSelector.length) {
+          $tickSelector.remove();
+          $(element).append(
+              '<i class="fas fa-times fa-2x availabilityMarker" data-date=' + date + '></i>'
+          );
+        } else if ($crossSelector.length) {
+          $crossSelector.remove();
+        } else {
+          $(element).append(
+              '<i class="fas fa-check fa-2x availabilityMarker" data-date=' + date + '></i>'
+          );
+        }
+      },
+    });
+    calendar.render();
+
+    getEvents(addEventsToCalendar);
+  });
+}());
--- includes.php
+++ includes.php
@@ -1,7 +1,8 @@
 <!-- JQuery -->
 <script src="https://code.jquery.com/jquery-3.4.1.js"
         integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+</script>
 
 <!-- PopperJS -->
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
@@ -9,19 +10,18 @@
         crossorigin="anonymous">
 </script>
 
-<!-- My Stylesheet -->
-<link rel="stylesheet" href="/styles/styles.css">
-
 <!-- Bootstrap -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+<link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous">
+      crossorigin="anonymous" />
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+</script>
 
 <!-- Fontawesome -->
-<link href='https://use.fontawesome.com/releases/v5.0.6/css/all.css' rel='stylesheet'>
+<link href='https://use.fontawesome.com/releases/v5.0.6/css/all.css' rel='stylesheet' />
 
 <!-- FullCalendar -->
 <link href='https://unpkg.com/@fullcalendar/core@4.3.1/main.min.css' rel='stylesheet' />
@@ -38,5 +38,11 @@
 <!-- day.js -->
 <script src="https://unpkg.com/dayjs@1.8.20/dayjs.min.js"></script>
 <script>
-  dayjs().format()
+  dayjs().format();
 </script>
+
+<!-- My Stylesheet -->
+<link rel="stylesheet" href="/styles/styles.css">
+
+<!-- JS Used to Render Calendar and provide functions for it -->
+<script type="text/javascript" src="./calendar.js"></script>
