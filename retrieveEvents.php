38f431c7be3f1f4a1cb0a8eb502540b4f8bff812
Tom Ford-Knowles <thomas.ford-knowles@hotmail.co.uk>
2020-03-25 11:34:41 +0000

    Revert "Updates to suit WP"
    This reverts commit bc9c45b923344ce10362abcb43da3bab8846cda0.

--- retrieveEvents.php
+++ retrieveEvents.php
@@ -3,8 +3,6 @@ include 'scraper.php';
 include 'eventSaver.php';
 include 'dbEventRetriever.php';
 include 'dbConnection.php';
-include 'displayEvents.php';
-include 'storeEvents.php';
 
 interface EventRetrievalInterface
 {
@@ -48,6 +46,57 @@ class DBEventRetriever implements EventRetrievalInterface
     }
 }
 
+class DisplayEvents
+{
+    private $retriever;
+
+    /**
+     * returnEvents constructor.
+     * @param EventRetrievalInterface $retriever
+     */
+    public function __construct(EventRetrievalInterface $retriever)
+    {
+        $this->retriever = $retriever;
+    }
+
+    public function returnEvents() : void
+    {
+        // You can guarantee $retriever will have method retrieveEvents
+        // as it is in the interface. Everything else is a mystery
+        $events = $this->retriever->retrieveEvents();
+        // do stuff with events
+        echo json_encode($events);
+    }
+}
+
+class SaveEvents
+{
+    private $retriever;
+
+    /**
+     * saveEvents constructor.
+     * @param EventRetrievalInterface $retriever
+     */
+    public function __construct(EventRetrievalInterface $retriever)
+    {
+        $this->retriever = $retriever;
+    }
+
+    public function saveEvents() : void
+    {
+        $pdo = $this->getPDO();
+        $events = $this->retriever->retrieveEvents();
+        $eventSaver = new EventSaver($pdo, $events);
+        $eventSaver->saveEvents();
+    }
+
+    private function getPDO()
+    {
+        $dbConnection = new PDOConnection();
+        return $dbConnection->pdo;
+    }
+}
+
 $retrieverSource = $_POST['source'] ?? '';
 
 if (empty($retrieverSource)) {
@@ -59,7 +108,7 @@ if (!in_array($retrieverSource, ['dbEvents', 'liveEvents'])) {
 }
 
 if ('liveEvents' === $retrieverSource) {
-    $saver = new StoreEvents(new LiveEventRetriever());
+    $saver = new SaveEvents(new LiveEventRetriever());
     $saver->saveEvents();
 }
 
